bcc964b8aa3770aa27d16ef539020e63da79589b
veloXtime <xhy20010806@gmail.com>
2024-04-12 02:32:00 -0400

    Clean up web branch

--- README.md
+++
@@ -1,3 +0,0 @@
-# Low-Poly-Effect-Parallel-Renderer
-
-Web page: [Low-Poly-Effect-Parallel-Renderer](https://veloxtime.github.io/Low-Poly-Effect-Parallel-Renderer/)
--- src/processing.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROCESSING_H
-#define PROCESSING_H
-#include "CImg.h"
-
-unsigned char* gaussianBlur(const unsigned char* inputImage, int width,
-                            int height, int channels);
-cimg_library::CImg<unsigned char> extractEdge(
-    cimg_library::CImg<unsigned char>& image);
-#endif
