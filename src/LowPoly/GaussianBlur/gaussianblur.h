8a4f4f3a5c852c82c022994f95aaec3ff7d513aa
Felicity Xu <xhy20010806@gmail.com>
2024-05-05 04:18:35 -0400

    Increase Gaussian blur radius and sigma

--- src/LowPoly/GaussianBlur/gaussianblur.h
+++ src/LowPoly/GaussianBlur/gaussianblur.h
@@ -8,8 +8,10 @@ using CImgBool = cimg_library::CImg<bool>;
 using CImgFloat = cimg_library::CImg<float>;
 
 const int THREAD_PER_BLK = 256;
-const double BLUR_SIGMA = 1;
-const int BLUR_RADIUS = 2;
+// const double BLUR_SIGMA = 1;
+const double BLUR_SIGMA = 2.3;
+// const int BLUR_RADIUS = 2;
+const int BLUR_RADIUS = 7;
 const int BLUR_WIDTH = 2 * BLUR_RADIUS + 1;
 
 // Creates a Gaussian kernel with input radius
