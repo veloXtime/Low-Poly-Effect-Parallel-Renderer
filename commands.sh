a8b0c59f62e433885333b3beb7a6f726b3589abe
Ao Wang <aow@andrew.cmu.edu>
2024-05-06 03:42:33 -0400

    update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,5 @@
+## test
 ./main ../images/emma.png
+
+## generate tar
+tar --exclude='./src/images' --exclude='./.git' --exclude='./.vscode' --exclude='./reports'  -cvzf low-poly-effect-parallel-renderer.tar.gz .
